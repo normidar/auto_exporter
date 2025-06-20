186bbfe94a66e07d836cf9e8ace53e46aba655b2
normidar <normidar7@gmail.com>
2025-06-20 01:42:35 -0700

    Remove example files and configurations for auto_exporter and flutter_example packages

--- packages/auto_exporter/example/flutter_example/lib/src/do_no_export.dart
+++
@@ -1,4 +0,0 @@
-import 'package:auto_exporter_annotation/auto_exporter_annotation.dart';
-
-@IgnoreExport()
-class DoNoExport {}
--- packages/auto_exporter/example/main.dart
+++
@@ -1,5 +0,0 @@
-// If you want to ignore a file, you can use @IgnoreExport() annotation
-
-class DoNoExport {}
-
-// See README.md for more details.
